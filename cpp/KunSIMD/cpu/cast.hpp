ab9f36635cc943851172efe1624acbcbc653ada8
Menooker <myjisgreat@live.cn>
2024-04-05 00:00:35 +0800

    f64

--- cpp/KunSIMD/cpu/cast.hpp
+++ cpp/KunSIMD/cpu/cast.hpp
@@ -1,5 +1,7 @@
 #include "f32x8.hpp"
 #include "s32x8.hpp"
+#include "f64x4.hpp"
+#include "s64x4.hpp"
 
 namespace kun_simd {
 template<typename T1, typename T2>
@@ -15,6 +17,18 @@ INLINE vec_s32x8 bitcast(vec_f32x8 v) {
     return _mm256_castps_si256(v.v);
 }
 
+
+template<>
+INLINE vec_f64x4 bitcast(vec_s64x4 v) {
+    return _mm256_castsi256_pd(v.v);
+}
+
+
+template<>
+INLINE vec_s64x4 bitcast(vec_f64x4 v) {
+    return _mm256_castpd_si256(v.v);
+}
+
 template<typename T1, typename T2>
 T1 cast(T2 v);
 
@@ -27,5 +41,16 @@ template<>
 INLINE vec_s32x8 cast(vec_f32x8 v) {
     return _mm256_cvtps_epi32(v.v);
 }
+
+template<>
+INLINE vec_f64x4 cast(vec_s64x4 v) {
+    return _mm256_cvtepi64_pd(v.v);
+}
+
+template<>
+INLINE vec_s64x4 cast(vec_f64x4 v) {
+    return _mm256_cvtpd_epi64(v.v);
+}
+
 
 }
