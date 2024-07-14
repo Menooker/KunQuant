e69728112354104b4b179cc0eb792f6ec59b3aaa
Menooker <myjisgreat@live.cn>
2024-07-14 21:46:06 +0800

    fix

--- tests/tests_avx512.sh
+++ tests/tests_avx512.sh
@@ -1,7 +1,7 @@
 set -e
 echo "KunQuant compiler tests"
 echo "KunQuant alpha101 tests"
-python tests/test_alpha101.py
+sde -clx -env PYTHONPATH "$PYTHONPATH" -- python tests/test_alpha101.py
 echo "KunQuant alpha158 tests"
-python ./tests/test_alpha158.py --inputs /tmp/input.npz --ref /tmp/alpha158.npz
+sde -clx -env PYTHONPATH "$PYTHONPATH" -- python ./tests/test_alpha158.py --inputs /tmp/input.npz --ref /tmp/alpha158.npz
 echo "All test done"
