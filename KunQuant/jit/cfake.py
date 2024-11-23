51d11c3b332eafa9e9117e2e553f8675c504ff10
Menooker <myjisgreat@live.cn>
2024-11-23 21:24:15 +0800

    allow select compiler

--- KunQuant/jit/cfake.py
+++ KunQuant/jit/cfake.py
@@ -5,6 +5,8 @@ from typing import List, Tuple
 import KunRunner
 from KunQuant.Driver import compileit as driver_compileit
 from KunQuant.Stage import Function
+from KunQuant.passes import Util
+import timeit
 
 _cpp_root = os.path.join(os.path.dirname(__file__), "..", "..", "cpp")
 _include_path = [_cpp_root]
@@ -14,6 +16,8 @@ def call_cpp_compiler(source: str, compiler: str, options: List[str], tempdir: s
     with open(inpath, 'w') as f:
         f.write(source)
     outpath = os.path.join(tempdir, "1.so")
+    if Util.jit_debug_mode:
+        print("[KUN_JIT] temp jit files:", inpath, outpath)
     cmd = [compiler] + options + [inpath, "-o", outpath]
     subprocess.check_call(cmd, shell=False)
     return outpath
@@ -24,7 +28,20 @@ def compile_cpp_and_load(source: str, tempdir: str, compiler: str, options: List
         lib = KunRunner.Library.load(outdir)
         return lib
 
-def compileit(f: Function, module_name: str, tempdir: str = None, **kwargs) -> Tuple[KunRunner.Library, KunRunner.Module]:
-    src = driver_compileit(f, module_name, **kwargs)
-    lib = compile_cpp_and_load(src, tempdir, "g++", ["-std=c++11", "-O2", "-shared", "-fPIC", "-march=native"] + [f"-I{v}" for v in _include_path])
+def compileit(f: Function, module_name: str, compiler: str = "g++", tempdir: str = None, **kwargs) -> Tuple[KunRunner.Library, KunRunner.Module]:
+    timeit.timeit()
+    lib = None
+    src = None
+    def kuncompile():
+        nonlocal src
+        src = driver_compileit(f, module_name, **kwargs)
+    def dowork():
+        nonlocal lib
+        lib = compile_cpp_and_load(src, tempdir, compiler, ["-std=c++11", "-O2", "-shared", "-fPIC", "-march=native"] + [f"-I{v}" for v in _include_path])
+    if Util.jit_debug_mode:
+        print("[KUN_JIT] Source generation takes ", timeit.timeit(kuncompile, number=1), "s")
+        print("[KUN_JIT] C++ compiler takes ", timeit.timeit(dowork, number=1), "s")
+    else:
+        kuncompile()
+        dowork()
     return lib, lib.getModule(module_name)
