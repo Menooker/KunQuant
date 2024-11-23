98e88ba41fcc24c3436239331dee1b49e0c1cfa9
Menooker <myjisgreat@live.cn>
2024-11-23 22:43:27 +0800

    keep files

--- KunQuant/jit/cfake.py
+++ KunQuant/jit/cfake.py
@@ -22,22 +22,34 @@ def call_cpp_compiler(source: str, compiler: str, options: List[str], tempdir: s
     subprocess.check_call(cmd, shell=False)
     return outpath
 
-def compile_cpp_and_load(source: str, tempdir: str, compiler: str, options: List[str]) -> KunRunner.Library:
-    with tempfile.TemporaryDirectory(dir=tempdir) as tmpdirname:
+class _fake_temp:
+    def __init__(self, dir) -> None:
+        self.dir = dir
+
+    def __enter__(self):
+        return self.dir
+
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        pass
+
+def compile_cpp_and_load(source: str, tempdir: str, compiler: str, options: List[str], keep_files: bool) -> KunRunner.Library:
+    tempclass = _fake_temp if keep_files else tempfile.TemporaryDirectory 
+    with tempclass(dir=tempdir) as tmpdirname:
         outdir = call_cpp_compiler(source, compiler, options, tmpdirname)
         lib = KunRunner.Library.load(outdir)
         return lib
 
-def compileit(f: Function, module_name: str, compiler: str = "g++", tempdir: str = None, **kwargs) -> Tuple[KunRunner.Library, KunRunner.Module]:
-    timeit.timeit()
+def compileit(f: Function, module_name: str, compiler: str = "g++", tempdir: str = None, keep_files: bool = False, **kwargs) -> Tuple[KunRunner.Library, KunRunner.Module]:
     lib = None
     src = None
+    if keep_files and not tempdir:
+        raise RuntimeError("if keep_files=True, tempdir should not be empty")
     def kuncompile():
         nonlocal src
         src = driver_compileit(f, module_name, **kwargs)
     def dowork():
         nonlocal lib
-        lib = compile_cpp_and_load(src, tempdir, compiler, ["-std=c++11", "-O2", "-shared", "-fPIC", "-march=native"] + [f"-I{v}" for v in _include_path])
+        lib = compile_cpp_and_load(src, tempdir, compiler, ["-std=c++11", "-O2", "-shared", "-fPIC", "-march=native"] + [f"-I{v}" for v in _include_path], keep_files)
     if Util.jit_debug_mode:
         print("[KUN_JIT] Source generation takes ", timeit.timeit(kuncompile, number=1), "s")
         print("[KUN_JIT] C++ compiler takes ", timeit.timeit(dowork, number=1), "s")
