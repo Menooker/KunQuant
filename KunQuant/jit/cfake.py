1eee7600fa3ee0f261f504aef86d01108cb4f544
Menooker <myjisgreat@live.cn>
2024-11-23 12:55:14 +0000

    add test

--- KunQuant/jit/cfake.py
+++ KunQuant/jit/cfake.py
@@ -6,6 +6,9 @@ import KunRunner
 from KunQuant.Driver import compileit as driver_compileit
 from KunQuant.Stage import Function
 
+_cpp_root = os.path.join(os.path.dirname(__file__), "..", "..", "cpp")
+_include_path = [_cpp_root]
+
 def call_cpp_compiler(source: str, compiler: str, options: List[str], tempdir: str) -> str:
     inpath = os.path.join(tempdir, "1.cpp")
     with open(inpath, 'w') as f:
@@ -16,12 +19,12 @@ def call_cpp_compiler(source: str, compiler: str, options: List[str], tempdir: s
     return outpath
 
 def compile_cpp_and_load(source: str, tempdir: str, compiler: str, options: List[str]) -> KunRunner.Library:
-    with tempfile.TemporaryDirectory(dir=compiler) as tmpdirname:
-        outdir = call_cpp_compiler(source, tempdir, compiler, options, tmpdirname)
-        lib = KunRunner.Library.load(libpath)
+    with tempfile.TemporaryDirectory(dir=tempdir) as tmpdirname:
+        outdir = call_cpp_compiler(source, compiler, options, tmpdirname)
+        lib = KunRunner.Library.load(outdir)
         return lib
 
 def compileit(f: Function, module_name: str, tempdir: str = None, **kwargs) -> Tuple[KunRunner.Library, KunRunner.Module]:
     src = driver_compileit(f, module_name, **kwargs)
-    lib = compile_cpp_and_load(src, tempdir, ["-O2", "-shared", "-fPIC"])
+    lib = compile_cpp_and_load(src, tempdir, "g++", ["-std=c++11", "-O2", "-shared", "-fPIC", "-march=native"] + [f"-I{v}" for v in _include_path])
     return lib, lib.getModule(module_name)
