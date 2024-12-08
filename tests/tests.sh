f49f64bcd8410ec871aa24618f5d8da75b3da839
Menooker <myjisgreat@live.cn>
2024-12-08 21:00:42 +0800

    fix path

--- tests/tests.sh
+++ tests/tests.sh
@@ -10,7 +10,7 @@ echo "KunQuant alpha158 tests"
 python ./tests/test_alpha158.py --inputs /tmp/input.npz --ref /tmp/alpha158.npz --action run_avx2
 echo "KunQuant CAPI tests"
 python ./tests/gen_alpha101_stream.py /tmp/
-./build/temp.linux-x86_64-cpython-39/KunCApiTest ./build/lib.linux-x86_64-cpython-39/KunQuant/runner/libKunTest.so /tmp/alpha101_stream/alpha101_stream.so
+LD_LIBRARY_PATH=./build/lib.linux-x86_64-cpython-39/KunQuant/runner/:${LD_LIBRARY_PATH} ./build/temp.linux-x86_64-cpython-39/KunCApiTest ./build/lib.linux-x86_64-cpython-39/KunQuant/runner/libKunTest.so /tmp/alpha101_stream/alpha101_stream.so
 echo "KunQuant AVX512 alpha101 tests"
 python tests/test_alpha101.py compile_avx512
 sde -clx -env PYTHONPATH "$PYTHONPATH" -- python tests/test_alpha101.py run_avx512
