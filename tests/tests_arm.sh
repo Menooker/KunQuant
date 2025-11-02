3d3e127738a77fa0a4db373420db89cf6c4f1c41
Menooker <myjisgreat@live.cn>
2025-11-02 15:08:41 +0800

    macos

--- tests/tests_arm.sh
+++ tests/tests_arm.sh
@@ -10,5 +10,6 @@ python tests/test_alpha101.py arm
 #python ./tests/test_alpha158.py --inputs /tmp/input.npz --ref /tmp/alpha158.npz --action run_avx2
 echo "KunQuant CAPI tests"
 python ./tests/gen_alpha101_stream.py /tmp/
+ls ./build/lib.linux-aarch64-cpython-39/KunQuant/runner/
 LD_LIBRARY_PATH=./build/lib.linux-aarch64-cpython-39/KunQuant/runner/:${LD_LIBRARY_PATH} ./build/temp.linux-aarch64-cpython-39/KunCApiTest ./build/lib.linux-aarch64-cpython-39/KunQuant/runner/libKunTest.so /tmp/alpha101_stream/alpha101_stream.so
 echo "All test done"
