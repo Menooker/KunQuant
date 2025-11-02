1479e5f361204b99544b6371252797840e4890eb
Menooker <myjisgreat@live.cn>
2025-11-02 17:08:43 +0800

    更新 Table.hpp

--- cpp/KunSIMD/cpu/Table.hpp
+++ cpp/KunSIMD/cpu/Table.hpp
@@ -9,8 +9,8 @@ struct LogLookupTable {
     KUN_API static const T logr_table[32];
 };
 
-template <> const double LogLookupTable<double>::r_table[32];
-template <> const double LogLookupTable<double>::logr_table[32];
-template <> const float LogLookupTable<float>::r_table[32];
-template <> const float LogLookupTable<float>::logr_table[32];
+extern template <> const double LogLookupTable<double>::r_table[32];
+extern template <> const double LogLookupTable<double>::logr_table[32];
+extern template <> const float LogLookupTable<float>::r_table[32];
+extern template <> const float LogLookupTable<float>::logr_table[32];
 }
