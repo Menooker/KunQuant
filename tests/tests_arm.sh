d21268ceaa2f287ad00a606bb15ca0b6f4c54ffa
Yijie Mei <yijiem@nvidia.com>
2026-05-14 22:46:45 -0700

    fix test script

--- tests/tests_arm.sh
+++ tests/tests_arm.sh
@@ -28,9 +28,9 @@ python ./tests/gen_alpha101_stream.py /tmp/
 
 
 if [ "$OS" = "Linux" ]; then
-    export LD_LIBRARY_PATH=./build/lib.${SYSTEM_TRIPPLE}-cpython-39/KunQuant/runner/:${LD_LIBRARY_PATH}
+    export LD_LIBRARY_PATH=./build/lib.${SYSTEM_TRIPPLE}-cpython-312/KunQuant/runner/:${LD_LIBRARY_PATH}
 elif [ "$OS" = "Darwin" ]; then
-    export DYLD_LIBRARY_PATH=./build/lib.${SYSTEM_TRIPPLE}-cpython-39/KunQuant/runner/:${DYLD_LIBRARY_PATH}
+    export DYLD_LIBRARY_PATH=./build/lib.${SYSTEM_TRIPPLE}-cpython-312/KunQuant/runner/:${DYLD_LIBRARY_PATH}
 fi
-./build/temp.${SYSTEM_TRIPPLE}-cpython-39/KunCApiTest ./build/lib.${SYSTEM_TRIPPLE}-cpython-39/KunQuant/runner/libKunTest.${DYN_LIB_EXT} /tmp/alpha101_stream/alpha101_stream.${DYN_LIB_EXT}
+./build/temp.${SYSTEM_TRIPPLE}-cpython-312/KunCApiTest ./build/lib.${SYSTEM_TRIPPLE}-cpython-312/KunQuant/runner/libKunTest.${DYN_LIB_EXT} /tmp/alpha101_stream/alpha101_stream.${DYN_LIB_EXT}
 echo "All test done"
