e5a03cf2a34e7acc44e5823138101df63967365d
Menooker <myjisgreat@live.cn>
2026-05-15 20:44:41 +0800

    add talib atr sar (#76)

--- tests/tests.sh
+++ tests/tests.sh
@@ -4,6 +4,8 @@ python tests/test.py
 python tests/test2.py
 echo "KunQuant runtime tests"
 python tests/test_runtime.py
+echo "KunQuant ta-lib indicator tests"
+python tests/test_talib.py
 echo "KunQuant stream tests"
 python tests/test_stream.py
 echo "KunQuant runtime tests (AVX)"
@@ -14,7 +16,7 @@ echo "KunQuant alpha158 tests"
 python ./tests/test_alpha158.py --inputs /tmp/input.npz --ref /tmp/alpha158.npz --action run_avx2
 echo "KunQuant CAPI tests"
 python ./tests/gen_alpha101_stream.py /tmp/
-LD_LIBRARY_PATH=./build/lib.linux-x86_64-cpython-39/KunQuant/runner/:${LD_LIBRARY_PATH} ./build/temp.linux-x86_64-cpython-39/KunCApiTest ./build/lib.linux-x86_64-cpython-39/KunQuant/runner/libKunTest.so /tmp/alpha101_stream/alpha101_stream.so
+LD_LIBRARY_PATH=./build/lib.linux-x86_64-cpython-312/KunQuant/runner/:${LD_LIBRARY_PATH} ./build/temp.linux-x86_64-cpython-312/KunCApiTest ./build/lib.linux-x86_64-cpython-312/KunQuant/runner/libKunTest.so /tmp/alpha101_stream/alpha101_stream.so
 echo "KunQuant AVX512 alpha101 tests"
 python tests/test_alpha101.py compile_avx512
 sde -clx -env PYTHONPATH "$PYTHONPATH" -- python tests/test_alpha101.py run_avx512
